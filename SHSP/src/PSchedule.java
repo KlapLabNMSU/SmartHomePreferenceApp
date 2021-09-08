2d6f62156b203fccdc87f697d66dd5a2844c1606
tplatt <theop@nmsu.edu>
2021-09-08 16:34:35 -0600

    Started SHSP

---
+++ SHSP/src/PSchedule.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PSchedule {
+	private int[]    A; // A is a set of appliances (or devices), usually written as the set of integers {1, . . . , |A|}.
+	private double[] E; // E = (e1, e2, ..., e|A|) is a vector of positive real numbers, where each ei represents the energy consumption of device i.
+	private int[]    T; // T is a set of time slots, usually written as the set of integers {1, . . . , |T|}.
+	//TODO
+	
+	
+	
+	
+}//end class PSchedule
---
+++ SHSP/src/SHSP.java
@@ -0,0 +1,20 @@
+//Smart Home Scheduling Problem (SHSP) is a java program based on the DFS algorithm defined in the 
+//research paper "A Scheduler for Smart Homes with Probabilistic User Preferences" (V. Nguyen et al.)
+
+public class SHSP {
+
+	public void SHSP(PSchedule P) {
+		//TODO
+	}//end function SHSP
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		//TODO
+
+	}//end function main
+
+}//end class SHPS
